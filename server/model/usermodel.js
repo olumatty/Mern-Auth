29f4e3a18955a1eb80c5740a9e9254398c6f6f95
Matthew Adebayo <olumatty@gmail.com>
2024-12-11 13:39:17 +0100

    second commit

---
+++ server/model/usermodel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{type:string, required: true},
+    email:{type: string, required:true, unique: true},
+    password:{type: string, required: true},
+    verifyOtp:{type:string, default:''},
+    
+
+})
--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/mongodb");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
