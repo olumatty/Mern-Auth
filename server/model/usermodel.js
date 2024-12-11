47516a1948ae1de2dbbf173b97149aecb701b8c1
Matthew Adebayo <olumatty@gmail.com>
2024-12-11 19:38:04 +0100

    Third commit

--- server/model/usermodel.js
+++ server/model/usermodel.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{type:string, required: true},
-    email:{type: string, required:true, unique: true},
-    password:{type: string, required: true},
-    verifyOtp:{type:string, default:''},
-    
-
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp:{type: String, default: ''},
+    verifyOtpExpireAt: {type: Number, default: 0},
+    isAccountVerified:{type: Boolean, default:false},
+    resetOtp:{type: String, default: ''},
+    resetOtpExpireAt:{type:Number, default:0}
 })
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema)
+
+module.export = userModel
