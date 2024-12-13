0935bccb299a4e372433b4b94d8f09b56a46d7d9
Matthew Adebayo <olumatty@gmail.com>
2024-12-13 14:22:37 +0100

    Fourth commit

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -18,6 +18,19 @@ export const register = async (req,res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
+        const user = new userModel({name, email, password:hashedPassword});
+
+        await user.save();
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+
+        res.cookie('token', token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? 'none' :'strict',
+            maxAge: 7 * 24 * 60 * 60   
+        });
+
     }
     catch (error) {
         res.json({success:false, message:error.message})
