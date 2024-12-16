f0e9b5309392cf9cc149d7ff9c4d83ea5fd00bd0
Matthew Adebayo <olumatty@gmail.com>
2024-12-16 23:45:00 +0100

    sixth commit

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -18,7 +18,7 @@ export const register = async (req,res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = new userModel({name, email, password:hashedPassword});
+        const user = new userModel({name, email, password: hashedPassword});
 
         await user.save();
 
@@ -30,6 +30,8 @@ export const register = async (req,res) => {
             sameSite: process.env.NODE_ENV === "production" ? 'none' :'strict',
             maxAge: 7 * 24 * 60 * 60   
         });
+
+        return res.json({success:true});
 
     }
     catch (error) {
@@ -49,18 +51,43 @@ export const login = async (req, res) => {
         const user = await userModel.findOne({email});
 
         if (!user){
-            return res.json({succedd: false, message:"Invalid Email"})
+            return res.json({success: false, message:"Invalid Email"})
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
 
         if(!isMatch) {
-
+            return res.json({success: false, message:"Invalid Password"})
         }
-
         
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+
+        res.cookie('token', token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? 'none' :'strict',
+            maxAge: 7 * 24 * 60 * 60   
+        });
+
+        return res.json({success:true});
     }
     catch(error){
-        return res.json({success: false , message: errror.message})
+        return res.json({success: false , message: error.message})
+    }
+}
+
+export const logout = async (req, res) => {
+    try{
+        res.clearCookie('token',{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? 'none' :'strict',
+            maxAge: 7 * 24 * 60 * 60   
+        });
+
+        return res.json({success:true, message:'Logged Out'});
+    }
+    catch(error){
+        return res.json({success: false , message: error.message})
     }
 }
