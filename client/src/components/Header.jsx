0602dcd3cc10bc5dd648e21cebc0c227933aa836
Matthew Adebayo <olumatty@gmail.com>
2025-01-13 15:36:55 +0100

    Header commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={assets.header_img} alt='' className='w-36 h-36 rounded-full mb-6'/>
+      <h1>Hey Developer <img src={assets.hand_wave} alt=''/> </h1>
+    </div>
+  )
+}
+
+export default Header
